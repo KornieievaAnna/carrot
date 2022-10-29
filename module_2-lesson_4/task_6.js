11ade25e3e945bd873dfd45053eb84b9a66d4af8
Iryna Artymuk <artimyk@ukr.net>
2022-10-29 15:57:04 +0300

    task_6

--- module_2-lesson_4/task_6.js
+++ module_2-lesson_4/task_6.js
@@ -1,1 +1,25 @@
+// Напиши функцію findLargestNumber(numbers) яка шукає найбільше число в масиві.
 
+// ``js function findLargestNumber(numbers) {}
+
+// console.log(findLargestNumber([2, 17, 94, 1, 23, 37]))); // 94 console.log(findLargestNumber([49, 4, 7, 83, 12]))); // 83 ``
+
+
+
+
+function findLargestNumber(numbers) {
+    let LargestNumber = numbers[0];  // припустим перше значення найбільше 
+    for (let i = 0; i < numbers.length; i += 1)    // перебираєм масив якщо наступне знвачення більше перезаписуєм значення в змінній 
+        if (numbers[i] > LargestNumber) {
+            LargestNumber = numbers[i]
+          
+        }
+        return LargestNumber                
+}
+
+let LargestNumber1 = findLargestNumber([2, 17, 94, 1, 23, 37])
+let LargestNumber2 = findLargestNumber([49, 4, 7, 83, 12])
+
+
+    console.log(LargestNumber1 )
+    console.log(LargestNumber2)
