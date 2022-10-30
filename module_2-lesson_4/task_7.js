be8b1e8349197fd7fbda2e121113f25551552195
Iryna Artymuk <artimyk@ukr.net>
2022-10-30 08:33:38 +0300

    Update task_7.js

--- module_2-lesson_4/task_7.js
+++ module_2-lesson_4/task_7.js
@@ -3,9 +3,6 @@
 // Усі аргументи будуть лише числами.
 
 
-// console.log(calAverage(1, 2, 3, 4));// 2.5
-// console.log(calAverage(14, 8, 2));// 8
-// console.log(calAverage(27, 43, 2, 8, 36)); // 23.2 ``
 
 let total = 0
 let numbersArray = [];
@@ -30,5 +27,8 @@ function calAverage( ) {
 
 calAverage(1, 2, 3, 4);// 2.5
 calAverage((14, 8, 2));// 8
-calAverage((27, 43, 2, 8, 36)); // 23.2 
+calAverage((27, 43, 2, 8, 36)); // 23.2
+
+
+
 
