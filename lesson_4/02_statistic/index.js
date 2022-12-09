1a187db5d8a2b11db6e7a2759011ba923c96e148
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2022-12-09 21:48:43 +0200

    added task 2 lesson 4

--- lesson_4/02_statistic/index.js
+++ lesson_4/02_statistic/index.js
@@ -6,13 +6,32 @@
 // Для роботи з класами рекомендується використовувати classList
 
 function collectDOMStat(root) {
+  const nods = root.childNodes 
+  console.log(nods)
+
   const stat = {
     tags: {},
     classes: {},
     texts: 0,
   };
+  for (const elem of nods) {
+    if (elem.nodeType === 3) {
+      stat.texts += 1;   
+    } else if (elem.nodeType === 1)
+    {
+      const tag = elem.tagName
+      if (stat.tags[tag]) {
+        stat.tags[tag]+=1
+      } else { stat.tags[tag] = 1 } 
 
+      const classes = elem.classList 
+      for (const el of classes) {
+        if (stat.classes[el]) {
+           stat.classes[el]+=1
+      } else { stat.classes[el] = 1 } 
+        } 
+    }  
+}
   return stat;
 }
-
 console.log(collectDOMStat(document.querySelector('.container')))
