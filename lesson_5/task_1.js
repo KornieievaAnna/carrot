53f66d358614f507cbd9ecf31c0dee71148f7051
Yaroslav Ashchaulov <y.ashchaulov@gmail.com>
2022-12-23 20:55:07 +0200

    added task 1

--- lesson_5/task_1.js
+++ lesson_5/task_1.js
@@ -1,3 +1,11 @@
 // Створіть функцію, яка отримує функцію як аргумент і виконує її через 2 секунди
 
-function delayFunction(callback) {}
+function delayFunction(callback) {
+    setTimeout(callback, 2000)
+}
+
+function foo(){
+    console.log('Hello world');
+};
+
+delayFunction(foo);
